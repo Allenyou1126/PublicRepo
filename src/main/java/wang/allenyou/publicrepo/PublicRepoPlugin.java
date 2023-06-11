16bb15fe448c9f2790f364f9441af89e5b2d1039
Allenyou <allenyou1126@gmail.com>
2023-06-11 10:41:45 +0800

    Initial commit and create project

---
+++ src/main/java/wang/allenyou/publicrepo/PublicRepoPlugin.java
@@ -0,0 +1,17 @@
+package wang.allenyou.publicrepo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PublicRepoPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		super.onEnable();
+
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+
+	}
+}
